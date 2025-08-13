d129645dca5bfbb699995dfaef1fa6e60f7d0f3a
Ilirjan Rexho <61479363+ilrexho2011@users.noreply.github.com>
2025-08-13 13:56:41 +0200

    Update server.js

--- 01  - Creation of a Web App/ui5/webapp/php/server.js
+++ 01  - Creation of a Web App/ui5/webapp/php/server.js
@@ -9,9 +9,9 @@ app.use(bodyParser.json());
 
 const db = mysql.createConnection({
   host: 'localhost:3306',
-  user: 'ilrexho',
+  user: 'xxxxxxxxxxxxxxx',
   password: 'xxxxxxxxxxxxxxxxxxxxx',
-  database: 'albsale-vlora'
+  database: 'xxxxxxxxxxxxxxxxxxx'
 });
 
 db.connect(err => {
@@ -42,3 +42,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
